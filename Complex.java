3580e9a0ef747e09f47a8e55609bffefd317b362
Adam Mahameed <23741427+Athl1n3@users.noreply.github.com>
2018-06-07 17:23:08 +0300

    Add files via upload

--- Complex.java
+++ Complex.java
@@ -1,6 +1,4 @@
-
 public class Complex {
-
 	private double Re;
 	private double Im;
 
@@ -34,32 +32,82 @@ public class Complex {
 	}
 	
 	public Complex minus(Complex w) { //  (a1 – a2 + i (b1 – b2) )
-		return new Complex(w.Re - this.Re, w.Im - this.Im);
+		return new Complex(this.Re - w.Re, this.Im - w.Im);
 	}
 	
 	//(a.Re * a.Im - w.Re * w.Im) + i (a.Re * w.Im + a.Im * w.Re) )
 	public Complex times(Complex w) {
-		Complex a = this;
-		double Re = a.Re * a.Im - w.Re * w.Im;
-		double Im = a.Re * w.Im + a.Im * w.Re;
-		return new Complex(Re, Im);
-	}
 	
-	public Complex div(Complex w) {// ((a.Re * a.Im - w.Re * w.Im) + i (a.Re * w.Im + a.Im * w.Re)) / (w.Re^2+ a.Im^2)
-		Complex a = this;
-		double Re = (a.Re * a.Im + w.Re * w.Im) / (Math.pow(w.Re, 2) + Math.pow(a.Im, 2));
-		double Im = (a.Re * w.Im - a.Im * w.Re) / (Math.pow(w.Re, 2) + Math.pow(a.Im, 2));
-		return new Complex(Re, Im);
+		double Re = (this.Re*w.Re)-(this.Im*w.Im);
+		double Im = (this.Re*w.Im)+(this.Im*w.Re);
+		return new Complex(Re,Im);
 	}
-	
-	public String toString()
-	{
-		return String.format("%.3f + %.3fi", this.Re < 0.001 ? 0 : this.Re, this.Im < 0.001 ? 0 : this.Im);
-	}
-	
-	public boolean equals(Object o) {
-		Complex w = (Complex) o;
-		return this.Re == w.Re && this.Im == w.Im;
-	}
+	 
+	 
+	 
+	 public Complex div(Complex w) 
+	 {
+		Complex t= w.times(w.conj());
+		Complex n = new Complex(1/t.Re,0);
+		return this.times(n.times(w.conj()));
+	 }
+	 
+	 
+	 
+	 public String toString()
+	 {
+		 if(this.Re<0.001&&this.Re>-0.001&&this.Im<0.001&&this.Im>-0.001)
+			 return "0.000";
+ 
+		 if(this.Im<0.001&&this.Im>-0.001)
+			 return String.format("%.3f", this.Re);
+		 if(this.Re<0.001&&this.Re>-0.001)
+			 return String.format("%.3f", this.Im)+"i";
+		 return String.format("%.3f", this.Re)+" + "+String.format("%.3f", this.Re)+"i";
+	 }
+	 
+	 
+	 public boolean equals(Object o)
+	 {
+		 String text;
+		 
+		  text = String.format("%.3f",this.Re);
+		 double Re = Double.parseDouble(text);
+		
+		 text = String.format("%.3f",this.Im );
+		 double Im = Double.parseDouble(text);
+		  
+		 text = String.format("%.3f",((Complex)o).Re );
+		 double oRe = Double.parseDouble(text);
+		 
+		 text = String.format("%.3f",((Complex)o).Im );
+		 double oIm = Double.parseDouble(text);
+		 
+
+		 
+		 
+		 if((Re != oRe)||(Im != oIm))
+			 return false;
+		 return true;
+		 
+		 
+		 
+		 
+	 }
+	 
+	/* public Complex power(Complex o)
+	 {
+		 
+		 double p = this.mod();
+		 double theta = Math.atan(this.y/this.x);
+		 double pPowA2 = Math.pow(p,o.x );
+		 double ePow = Math.pow(Math.exp(1),(-1)*o.y*theta);
+		 double x = Math.cos(o.y*Math.log(p)+o.y*theta);
+		 double y = Math.sin(o.y*Math.log(p)+o.y*theta);
+		 
+		 Complex t = Complex()
+		 
+	 }*/
+	 
 }
 
